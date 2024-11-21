386730a567fc94c9dc6b091a9b91a4c6d00446da
3CRhayssa <00001113031396sp@al.educacao.sp.gov.br>
2024-11-21 10:34:28 -0300

    Add files via upload

---
+++ Ferramenta de estudo - Rhayssa - 3F/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O HTML é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Itália?',
+    'A capital da Itália é Roma'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz borboleta em Inglês?',
+    'Borboleta em inglês é Butterfly'
+)
